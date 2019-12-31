35f77c712cd19a424bd5f5510ff521b1ed4f49d0
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2019-12-31 09:26:43 +0200

    Use Grommet with theme.

--- StarCommander/ClientApp/src/App.tsx
+++ StarCommander/ClientApp/src/App.tsx
@@ -1,16 +1,18 @@
-import * as React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
-
-import './custom.css'
+import * as React from "react";
+import { Route } from "react-router";
+import { Grommet } from "grommet";
+import { grommet } from "grommet/themes";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Counter from "./components/Counter";
+import FetchData from "./components/FetchData";
 
 export default () => (
-    <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-    </Layout>
+	<Grommet theme={grommet}>
+		<Layout>
+			<Route exact path="/" component={Home} />
+			<Route path="/counter" component={Counter} />
+			<Route path="/fetch-data/:startDateIndex?" component={FetchData} />
+		</Layout>
+	</Grommet>
 );
