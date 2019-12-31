cd752a8e30345db0378f9a609d931591504cfdf2
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2019-12-31 13:01:00 +0200

    Remove imports.

--- StarCommander/ClientApp/src/components/index.js
+++ StarCommander/ClientApp/src/components/index.js
@@ -1,6 +1,4 @@
-import Counter from "../routes/CounterPage";
-import FetchData from "../routes/FetchDataPage";
 import Layout from "./Layout";
 import NavMenu from "./NavMenu";
 
-export { Counter, FetchData, Layout, NavMenu };
+export { Layout, NavMenu };
