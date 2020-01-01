79b1d2b27aca07580333b69ded574293e5b1dbdc
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-01 11:30:27 +0200

    Define the SignUp API.

---
+++ StarCommander/Controllers/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StarCommander.Application.Services;
+using StarCommander.Shared.Model;
+
+namespace StarCommander.Controllers
+{
+	[Authorize]
+	[Produces("application/json")]
+	[Route("api/[controller]")]
+	public class PlayerController : ControllerBase
+	{
+		readonly IPlayerService playerService;
+
+		public PlayerController(IPlayerService playerService)
+		{
+			this.playerService = playerService;
+		}
+
+		[AllowAnonymous]
+		[HttpPost("player")]
+		public async Task<IActionResult> SignUp(Player player)
+		{
+			return Ok(await playerService.SignUp(player.CallSign, player.FirstName, player.LastName));
+		}
+	}
+}
