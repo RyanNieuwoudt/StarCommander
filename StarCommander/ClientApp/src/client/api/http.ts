a22bb8149f0943416d1ea49526d835464f4b4778
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-01 13:55:09 +0200

    Add http client.

---
+++ StarCommander/ClientApp/src/client/api/http.ts
@@ -0,0 +1,21 @@
+import * as R from "ramda";
+import axios from "axios";
+import { getConfig } from ".";
+
+const resolve = (promise: Promise<any>) =>
+	promise
+		.then((response: { data: any }) => response.data)
+		.catch((error: any) => {
+			throw R.path(["response", "data"], error) || {
+				message: "System offline, please try again later."
+			};
+		});
+
+export default {
+	delete: (url: string, token: string) =>
+		resolve(axios.delete(url, getConfig(token))),
+	get: (url: string, token: string, config: object | undefined) =>
+		resolve(axios.get(url, getConfig(token, config))),
+	post: (url: string, data = {}, token: string | undefined = undefined) =>
+		resolve(axios.post(url, data, getConfig(token)))
+};
