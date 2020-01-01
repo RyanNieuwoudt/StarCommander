698a331e83fb4688a1a4dc0a896823ea6e82ea94
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-01 14:36:31 +0200

    Define Action.

--- StarCommander/ClientApp/src/store/index.ts
+++ StarCommander/ClientApp/src/store/index.ts
@@ -12,6 +12,11 @@ export const sagas = {
 	auth: auth.rootSaga
 };
 
+export interface Action {
+	type: string;
+	payload: object | undefined;
+}
+
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
 // correctly typed to match your store.
 export interface AppThunkAction<TAction> {
