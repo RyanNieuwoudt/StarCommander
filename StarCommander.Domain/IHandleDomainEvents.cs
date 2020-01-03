c729010e897e5548359952c50ccea81feb82babe
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-03 11:45:54 +0200

    Define HandleDomainEvents.

---
+++ StarCommander.Domain/IHandleDomainEvents.cs
@@ -0,0 +1,10 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StarCommander.Domain
+{
+	public interface IHandleDomainEvents<in T> where T : IDomainEvent
+	{
+		Task Handle(T @event, CancellationToken cancellationToken);
+	}
+}
