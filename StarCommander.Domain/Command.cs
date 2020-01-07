dfb2e15c786c0724617617498d533ed123777096
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-07 12:40:03 +0200

    Add Command base class.

---
+++ StarCommander.Domain/Command.cs
@@ -0,0 +1,13 @@
+using System;
+using Newtonsoft.Json;
+
+namespace StarCommander.Domain
+{
+	[Serializable]
+	[JsonObject(MemberSerialization.OptIn)]
+	public abstract class Command : ICommand
+	{
+		[JsonProperty]
+		public string Type => GetType().FullName!;
+	}
+}
