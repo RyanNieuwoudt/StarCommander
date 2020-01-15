e441e6d4063cf62c1e3d4a4e07a4cb6e0a135d5d
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-15 06:51:10 +0200

    Remove unused code.

--- StarCommander.Application/CommandHandlers/IObey.cs
+++ StarCommander.Application/CommandHandlers/IObey.cs
@@ -2,11 +2,7 @@ using StarCommander.Domain;
 
 namespace StarCommander.Application.CommandHandlers
 {
-	public interface IObey
-	{
-	}
-
-	public interface IObey<in T> : IObey, IHandleCommands<T> where T : ICommand
+	public interface IObey<in T> : IHandleCommands<T> where T : ICommand
 	{
 	}
 }
--- StarCommander.Application/DomainEventHandlers/IWhen.cs
+++ StarCommander.Application/DomainEventHandlers/IWhen.cs
@@ -2,11 +2,7 @@ using StarCommander.Domain;
 
 namespace StarCommander.Application.DomainEventHandlers
 {
-	public interface IWhen
-	{
-	}
-
-	public interface IWhen<in T> : IWhen, IHandleDomainEvents<T> where T : IDomainEvent
+	public interface IWhen<in T> : IHandleDomainEvents<T> where T : IDomainEvent
 	{
 	}
 }
