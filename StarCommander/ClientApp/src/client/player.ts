8f8da620567b1874c8a73081c64928752bbab156
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-15 16:47:25 +0200

    Remove defaults and optionals.

--- StarCommander/ClientApp/src/client/player.ts
+++ StarCommander/ClientApp/src/client/player.ts
@@ -3,8 +3,10 @@ import { http } from "client/api";
 export const signIn = ({
 	callSign,
 	password
-}: { callSign?: string; password?: string } = {}) =>
-	http.post("api/player/session", { callSign, password });
+}: {
+	callSign: string;
+	password: string;
+}) => http.post("api/player/session", { callSign, password });
 
 export const signUp = ({
 	callSign,
@@ -12,16 +14,19 @@ export const signUp = ({
 	lastName,
 	password
 }: {
-	callSign?: string;
-	firstName?: string;
-	lastName?: string;
-	password?: string;
-} = {}) =>
+	callSign: string;
+	firstName: string;
+	lastName: string;
+	password: string;
+}) =>
 	http.post("api/player/player", { callSign, firstName, lastName, password });
 
 export const updateName = ({
 	firstName,
 	lastName,
 	token
-}: { firstName?: string; lastName?: string; token?: string } = {}) =>
-	http.post("api/player/name", { firstName, lastName }, token);
+}: {
+	firstName: string;
+	lastName: string;
+	token: string;
+}) => http.post("api/player/name", { firstName, lastName }, token);
