2877b0efc043edda68ecb839636366d84f6df573
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-17 15:05:19 +0200

    Fetch ship from commands.

--- StarCommander.Infrastructure.Persistence.Aggregate/Ships/ShipCommandRepository.cs
+++ StarCommander.Infrastructure.Persistence.Aggregate/Ships/ShipCommandRepository.cs
@@ -1,8 +1,11 @@
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using StarCommander.Domain.Messages;
+using StarCommander.Domain;
+using StarCommander.Domain.Players;
 using StarCommander.Domain.Ships;
+using Command = StarCommander.Domain.Messages.Command;
 
 namespace StarCommander.Infrastructure.Persistence.Aggregate.Ships
 {
@@ -18,9 +21,44 @@ namespace StarCommander.Infrastructure.Persistence.Aggregate.Ships
 		{
 			return (await GetDbSet()
 				.AsNoTracking()
-				.Where(e => e.Processed == null)
-				.OrderBy(e => e.Created)
+				.Where(c => c.Processed == null)
+				.OrderBy(c => c.Created)
 				.FirstOrDefaultAsync())?.ToDomain();
+		}
+
+		public async Task<Ship> Fetch(Reference<Ship> reference)
+		{
+			var commands = await GetDbSet()
+				.AsNoTracking()
+				.Where(c => c.TargetId == reference)
+				.OrderBy(c => c.Created)
+				.Select(c => c.ToDomain())
+				.ToListAsync();
+
+			var ship = Ship.Launch(Reference<Ship>.None, Reference<Player>.None);
+
+			foreach (var command in commands)
+			{
+				switch (command.Payload)
+				{
+					case LaunchShip launchShip:
+						ship = Ship.Launch(launchShip.Ship, launchShip.Captain);
+						break;
+					case SetHeading setHeading:
+						ship.NavigationComputer.SetHeading(command.Created, setHeading.Heading);
+						break;
+					case SetSpeed setSpeed:
+						ship.NavigationComputer.SetSpeed(command.Created, setSpeed.Speed);
+						break;
+				}
+			}
+
+			if (ship.Reference == Reference<Ship>.None)
+			{
+				throw new InvalidDataException($"Unable to fetch ship {reference}");
+			}
+
+			return ship;
 		}
 
 		protected override Messages.Command AddEntity()
