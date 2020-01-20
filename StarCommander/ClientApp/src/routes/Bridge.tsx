af1e54ca29cb30577d3b308a1ee3e4f3ee17eeca
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-20 09:56:42 +0200

    Use grid.

--- StarCommander/ClientApp/src/routes/Bridge.tsx
+++ StarCommander/ClientApp/src/routes/Bridge.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Box, Heading } from "grommet";
+import { Box, Grid, Heading } from "grommet";
 import { Layout } from "components";
 import { Helm } from "components/ship/helm";
 import { Navigation } from "components/ship/navigation";
@@ -9,9 +9,18 @@ export default function Bridge() {
 		<Layout>
 			<Box align="center" pad="large">
 				<Heading>Welcome, Commander.</Heading>
+			</Box>
+			<Grid
+				alignSelf="center"
+				columns={[
+					["medium", "full"],
+					["medium", "full"]
+				]}
+				gap="medium"
+			>
 				<Helm />
 				<Navigation />
-			</Box>
+			</Grid>
 		</Layout>
 	);
 }
