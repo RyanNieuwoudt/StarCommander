061da3409518fdd583b7e1634144301c46b90275
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-20 17:00:41 +0200

    Improve display.

--- StarCommander/ClientApp/src/components/ship/navigation/Navigation.tsx
+++ StarCommander/ClientApp/src/components/ship/navigation/Navigation.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Box, Heading, Text } from "grommet";
+import { Text } from "grommet";
+import styled from "styled-components/macro";
 import { shipPosition } from "selectors";
+
+const Circle = styled.div`
+	background-color: lightgray;
+	border-radius: 50%;
+	position: relative;
+`;
+
+const X = styled(Text)`
+	left: 80%;
+	position: absolute;
+	top: 50%;
+`;
+
+const Y = styled(Text)`
+	left: 50%;
+	position: absolute;
+	top: 10%;
+`;
 
 export default function Navigation() {
 	const position = useSelector(shipPosition);
@@ -13,11 +32,9 @@ export default function Navigation() {
 	const { x, y } = position;
 
 	return (
-		<Box>
-			<Heading>X</Heading>
-			<Text>{x}</Text>
-			<Heading>Y</Heading>
-			<Text>{y}</Text>
-		</Box>
+		<Circle>
+			<X>{x}</X>
+			<Y>{y}</Y>
+		</Circle>
 	);
 }
