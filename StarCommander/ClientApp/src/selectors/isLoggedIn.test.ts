34e60de78a58a24d2b03166d8950b8740da3725d
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-20 10:18:31 +0200

    Fix warnings.

--- StarCommander/ClientApp/src/selectors/isLoggedIn.test.ts
+++ StarCommander/ClientApp/src/selectors/isLoggedIn.test.ts
@@ -3,22 +3,22 @@ import { ApplicationState } from "store";
 
 describe("isLoggedOn", () => {
 	it("returns false if no auth state", () => {
-		const state: ApplicationState = { auth: undefined };
+		const state: ApplicationState = { auth: undefined, ship: {} };
 		expect(isLoggedIn(state)).toBe(false);
 	});
 
 	it("returns false if no token", () => {
-		const state: ApplicationState = { auth: {} };
+		const state: ApplicationState = { auth: {}, ship: {} };
 		expect(isLoggedIn(state)).toBe(false);
 	});
 
 	it("returns false if token is falsy", () => {
-		const state: ApplicationState = { auth: { token: "" } };
+		const state: ApplicationState = { auth: { token: "" }, ship: {} };
 		expect(isLoggedIn(state)).toBe(false);
 	});
 
 	it("returns true if token is truthy", () => {
-		const state: ApplicationState = { auth: { token: "token" } };
+		const state: ApplicationState = { auth: { token: "token" }, ship: {} };
 		expect(isLoggedIn(state)).toBe(true);
 	});
 });
