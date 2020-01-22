995dce94dbc566c25693fa21005a902196dc77a9
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2020-01-22 16:16:39 +0200

    Define IProjectWithKey.

---
+++ StarCommander.Infrastructure.Persistence.Projection/IProjectWithKey.cs
@@ -0,0 +1,11 @@
+namespace StarCommander.Infrastructure.Persistence.Projection
+{
+	public interface IProjectWithKey<T>
+	{
+		bool HasSamePrimaryKeyAs(T other);
+		IsToType IsTo(T other);
+		bool IsUpdateFor(T other);
+		bool RepresentsTheSameDataAs(T other);
+		T WithPrimaryKeyFrom(T other);
+	}
+}
