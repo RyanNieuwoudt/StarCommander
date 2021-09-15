5edb3ac3ab872240e7ab9e3639e613b95d92c2fd
Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>
2021-09-15 06:04:52 -0400

    Make readonly.
    Signed-off-by: Ryan Nieuwoudt <1231430+RyanNieuwoudt@users.noreply.github.com>

--- StarCommander.Domain/Reference.cs
+++ StarCommander.Domain/Reference.cs
@@ -20,7 +20,7 @@ namespace StarCommander.Domain
 	[JsonObject(MemberSerialization.OptIn)]
 	public readonly struct Reference<T> : IEquatable<Reference<T>> where T : IAggregate
 	{
-		public static Reference<T> None = new Reference<T>(Guid.Empty);
+		public static readonly Reference<T> None = new Reference<T>(Guid.Empty);
 		
 		[JsonConstructor]
 		public Reference(Guid id)
